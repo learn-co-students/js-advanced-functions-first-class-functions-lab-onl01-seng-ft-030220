c64b4db2b05b856e49bb4c172455587080adf335
marsha <marshaannon@gmail.com>
2020-10-26 22:17:53 -0400

    complete challenges

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = drivers => {return [drivers[0], drivers[1]]};
+
+const returnLastTwoDrivers = drivers => {return [drivers[drivers.length - 2], drivers[drivers.length - 1]]};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(n) {
+    return (fare) => {
+       return n * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selector) {
+    return selector(drivers)
+}
+    
